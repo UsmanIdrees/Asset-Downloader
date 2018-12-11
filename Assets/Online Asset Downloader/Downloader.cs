665e2c43dd5dd4e91561bc7e59bd6c5474b5d1aa
Usman Idrees <m.usman4057@gmail.com>
2018-12-11 15:18:12 +0500

    Initial commit

---
+++ Assets/Online Asset Downloader/Downloader.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Downloader : MonoBehaviour {
+
+	public string URL;
+
+	// Use this for initialization
+	public void download () {
+		StartCoroutine (Download());
+	}
+	
+	// Update is called once per frame
+	IEnumerator Download()
+	{
+		if (URL != null) 
+		{
+			// Link to url from where to download data
+			WWW www = new WWW (URL);
+			//Checking if still downloading
+			while (!www.isDone)
+			{
+				//logging the progress 
+				Debug.Log ("Downloading Assets " + www.progress);
+				yield return null;
+			}
+			//Getting the asset bundle from downloaded data
+			AssetBundle AB = www.assetBundle;
+			if (AB != null) 
+			{
+				//Checking if Loaded data has some values
+				//TODO Load Objects here
+				GameObject GO = AB.LoadAsset("Bit Gun") as GameObject;
+				Instantiate (GO);
+			} else 
+			{
+				Debug.Log ("Asset Does Not Found");
+			}
+			yield return null;
+		} 
+		else 
+		{
+			Debug.Log ("You Did not provide URL to download data");
+		}
+	}
+}
---
+++ Assets/Online Asset Downloader/Editor/AssetDownloader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class AssetDownloader : ScriptableObject
+{
+
+	[MenuItem("Assets/Build Asset Bundles/Normal")]
+	public static void BuildABsNone()
+	{
+		BuildPipeline.BuildAssetBundles("Assets/MyAssetBuilds", BuildAssetBundleOptions.None,BuildTarget.StandaloneWindows);
+	}
+
+	[MenuItem("Assets/Build Asset Bundles/Strict Mode ")]
+	public static void BuildABsStrict()
+	{
+		BuildPipeline.BuildAssetBundles("Assets/MyAssetBuilds", BuildAssetBundleOptions.StrictMode, BuildTarget.Android);
+	}
+}
