9d9c995b94b83a750ae25983418c4a56709da97e
Usman Idrees <m.usman4057@gmail.com>
2018-12-11 16:52:06 +0500

    Asset Downloading Plugin Completed
    This plugin is completed data is downloaded only one time then saved in  memory and loaded when necessary

--- Assets/Online Asset Downloader/Downloader.cs
+++ Assets/Online Asset Downloader/Downloader.cs
@@ -1,21 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using System.IO;
 
 public class Downloader : MonoBehaviour {
 
 	public string URL;
 
-	// Use this for initialization
+
+	private string DestinationTosave;
+	//private int datadownload = 0;
+
+	void Start()
+	{
+		DestinationTosave = Path.Combine (Application.persistentDataPath, "Data.unity3d");
+		Debug.Log (Directory.Exists(DestinationTosave) ? "true":"false");
+
+	}
 	public void download () {
 		StartCoroutine (Download());
 	}
-	
-	// Update is called once per frame
+
 	IEnumerator Download()
 	{
-		if (URL != null) 
+		if (URL != null && PlayerPrefs.GetInt ("datadownloaded") != 1) 
 		{
 			// Link to url from where to download data
 			WWW www = new WWW (URL);
@@ -27,22 +35,44 @@ public class Downloader : MonoBehaviour {
 				yield return null;
 			}
 			//Getting the asset bundle from downloaded data
-			AssetBundle AB = www.assetBundle;
-			if (AB != null) 
-			{
+			//AssetBundle AB = www.assetBundle;
+			//if (AB != null) 
+			//{
 				//Checking if Loaded data has some values
 				//TODO Load Objects here
-				GameObject GO = AB.LoadAsset("Bit Gun") as GameObject;
-				Instantiate (GO);
-			} else 
-			{
-				Debug.Log ("Asset Does Not Found");
-			}
+
+			Debug.Log ("Download Completed now sabving at " + DestinationTosave);
+				
+			byte[] DownloadedData = www.bytes;
+
+			File.WriteAllBytes (DestinationTosave, DownloadedData);
+			PlayerPrefs.SetInt ("datadownloaded", 1);
+			//GameObject GO = AB.LoadAsset("Bit Gun") as GameObject;
+				//Instantiate (GO);
+			//} else 
+			//{
+			//	Debug.Log ("Asset Does Not Found");
+			//}
 			yield return null;
 		} 
 		else 
 		{
 			Debug.Log ("You Did not provide URL to download data");
+			LoadAsset ();
+		}
+	}
+	public void LoadAsset()
+	{
+		
+		var assetBundle = AssetBundle.LoadFromFile (DestinationTosave);
+		if (assetBundle != null) 
+		{
+			GameObject GO = assetBundle.LoadAsset ("Bit Gun") as GameObject;
+			Instantiate (GO);
+
+		} else 
+		{
+			Debug.Log ("Asset Bundles is not found at " + DestinationTosave);
 		}
 	}
 }
